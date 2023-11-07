9ecb6fd4275b935fedf25136b8636cbf38e12c34
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 <iampatrickugo@gmail.com>
2023-11-07 17:16:04 +0100

    modified  mobile dropdown menu

--- src/components/main/Header/MobileHeaderLinks.tsx
+++ src/components/main/Header/MobileHeaderLinks.tsx
@@ -15,11 +15,11 @@ const MobileHeaderLinks:FC<Props> = ({setMobileToggled}) => {
 
   return (
     <div  className='flex justify-center items-center md:hidden w-screen h-screen absolute z-50 top-0 right-0 left-0 bottom-0 overflow-hidden bg-grey-light animate-[pulse_1s_ease-out_1]'>
-         <div onClick={()=> setMobileToggled(false)} className="absolute top-20 right-20">
+         <div onClick={()=> setMobileToggled(false)} className="absolute top-20 right-20 flex justify-center items-center">
            <GrClose size={30}/>
          </div>
          
-         <nav onClick={()=> setMobileToggled(false)} className="flex flex-col justify-between items-center text-green-primary text-lg font-bold gap-6 border border-green-primary rounded-md p-6">
+         <nav onClick={()=> setMobileToggled(false)} className="flex flex-col justify-between items-center text-green-primary text-lg font-bold gap-4 border border-green-primary rounded-md p-6">
               <Link className=' hover:underline hover:underline-offset-4 hover:text-green-primary py-2 animate-[bounce_0.1s_ease-out_2]' href='/' >HOME</Link>
               <Link className='hover:underline hover:underline-offset-4 hover:text-green-primary py-2 animate-[bounce_0.1s_ease-out_2]' href='/about' >ABOUT US</Link>
               <Link className='hover:underline hover:underline-offset-4 hover:text-green-primary py-2 animate-[bounce_0.1s_ease-out_2]' href='/contact' >CONTACT US</Link>
